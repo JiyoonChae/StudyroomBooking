6885e773817be751182bd5102ba02283b897251a
JiyoonChae <71802752+JiyoonChae@users.noreply.github.com>
2020-12-15 15:31:52 +0900

    Initial commit

---
+++ src/test/java/com/jy/sb7/SpringBoot7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.sb7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
